33205c170ababe71d3ab57cd044b5403047998ed
Wee Kee How <hrsolution@timeteccloud.com>
2025-03-06 17:37:03 +0800

    6/3/2025

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,13 +23,11 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('leads:update-status')->dailyAt('00:01'); // Runs daily at 12:01 AM
 
-        $schedule->command('follow-up:auto')->everyMinute(); //Runs weekly at Tuesday 10am
-
-        $schedule->command('facebook:fetch-leads')->everyMinute();  //Runs every minutes
+        $schedule->command('follow-up:auto')->weeklyOn(2, '10:00'); // Runs at Tuesday 10 AM
 
         $schedule->command('userleave:update')->everyThreeHours(); // Runs every 3 hours
 
-        $schedule->command('zoho:fetch-leads')->cron('*/15 6-21 * * *'); // Runs every 15 minutes between 6 AM and 9 PM
+        $schedule->command('zoho:fetch-leads')->everyFiveMinutes(); //Runs every 5 minutes
     }
 
     /**
